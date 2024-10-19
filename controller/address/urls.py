40f4d883956333de0c87d17a1b284138b71a0c6d
Enes Burak Özdemir <116081891+Dardalyan@users.noreply.github.com>
2024-10-19 21:28:39 +0300

    Readme file has been added.

--- controller/address/urls.py
+++ controller/address/urls.py
@@ -5,9 +5,9 @@ from .remove import RemoveAddresses
 from .update import UpdateAddresses
 
 urlpatterns = [
-    path('address/<int:address_id>/', ListAddresses.as_view()),
-    path('address/', ListAddresses.as_view()),
-    path('address/update/<int:address_id>/', UpdateAddresses.as_view()),
-    path('address/remove/<int:address_id>/', RemoveAddresses.as_view()),
+    path('address/<int:address_id>', ListAddresses.as_view()),
+    path('address', ListAddresses.as_view()),
+    path('address/update/<int:address_id>', UpdateAddresses.as_view()),
+    path('address/remove/<int:address_id>', RemoveAddresses.as_view()),
 
 ]
--- controller/jwt/urls.py
+++ controller/jwt/urls.py
@@ -2,5 +2,5 @@ from .JWTview import JWTView
 from django.urls import path
 
 urlpatterns = [
-    path('jwt-token/',JWTView.as_view(), name='jwt_token')
+    path('login/',JWTView.as_view(), name='login')
 ]
